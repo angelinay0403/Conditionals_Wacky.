b8d165e59b3eb84512cc8a6ea369be3953dd615e
angelinay0403 <angelinay0403@gmail.com>
2013-11-14 22:31:19 -0500

    Conditional_Wacky unfinished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+// Angelina Castro, 11/14/2013, Conditionals
+
+// Wacky Conditionals.
+
+//if a kid has to do homework, doing homework takes him 1 1/2 hour and the kid spend 3 hours watching TV, he comes out of school at 4 pm and goes to bed at 8 pm
+
+var homeWorktime = 1.5
+var tvHours = 3
+var hourstoSleep = 4
+//if the kid takes 1.5 hour doing home and 3 hours watching tv
+	if(tvHours + homeWorktime > hourstoSleep){
+	console.log("sorry, the kid has to watch less hours of T.V")
+{ 
